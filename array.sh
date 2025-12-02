c7b94ca44aa1c9dec694395eb613defee6bb4c7f
uday799 <udaysai299@gmail.com>
2025-12-02 18:13:44 +0530

    hiiiii

---
+++ array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+MOVIES={"pushpa" "rrr" "KGF"}
+echo "first movie is ${MOVIES[0]}"
+echo "first movie is ${MOVIES[1]}"
+echo "first movie is ${MOVIES[2]}
+echo "first movie is ${MOVIES[@]}
