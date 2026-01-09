acd033ecadc8693de97459892373be43303735b2
uday799 <udaysai299@gmail.com>
2026-01-09 09:09:39 +0530

    backup

--- backup-script.sh
+++ backup-script.sh
@@ -44,13 +44,13 @@ if [ -n "$FILES"]
 then
 echo "files to be zip is :$FILES"
 ZIP_FILE="$DES_DIR/app-logs-$TIMESTAMP.log"
-find $SOURCE_DIR -name ".log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
+find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
 if [ -f "$ZIP_FILE"]
 then
 echo "successfully created zip file for older than $DAYS"
 while read -r filepath
 do
-echo "files to be deleting are: $filepath"
+echo "files to be deleting are :$filepath"
 rm -rf $filepath
 done <<< $FILES
 else
