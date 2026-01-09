d272267e95bbe7d31eea51072f5cc0ed72ee1fae
uday799 <udaysai299@gmail.com>
2026-01-09 13:01:06 +0530

    backup

---
+++ monitoring.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+DISK_USAGE=$(df -hT | grep xfs )
+DISK_THRESHOLD=5
+MSG=""
+
+while read -r line
+
+do
+
+USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" f1)
+PARTITION=$(echo $ line | awk -F " " '{print $NF}' )
+echo "PARTITION is:$PARTITION,USAGE is: $USAGE"
+
+if [ $USAGE -ge $DISK_THRESHOLD ]
+then
+MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
+fi
+done <<< DISK_USAGE
+echo "MSG is:$MSG"
+
+echo "$MSG" | mutt -s "High Disk Usage" udaysai299@gmail.com
