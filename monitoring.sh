e2b2d7ec9a2bbcfd77b61b45fbd92af4b06271af
uday799 <udaysai299@gmail.com>
2026-01-09 13:08:44 +0530

    backup

--- monitoring.sh
+++ monitoring.sh
@@ -6,16 +6,13 @@ MSG=""
 while read -r line
 
 do
-
 USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" f1)
 PARTITION=$(echo $ line | awk -F " " '{print $NF}')
 echo "PARTITION is:$PARTITION,USAGE is: $USAGE"
-
 if [ $USAGE -ge $DISK_THRESHOLD ]
 then
 MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
 fi
 done <<< DISK_USAGE
 echo -e "MSG is:$MSG"
-
 echo "$MSG" | mutt -s "High Disk Usage" udaysai299@gmail.com
